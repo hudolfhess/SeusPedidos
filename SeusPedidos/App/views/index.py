a8a7a4555b1dddcdda6a13f17f69f6f2177c7e3c
Hudolf Hess <hudolf@meuspedidos.com.br>
2015-09-16 18:14:12 -0300

    Initial commit

---
+++ SeusPedidos/App/views/index.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def ini(request):
+    return HttpResponse("Hello World")
+
+'''
+
+class index:
+    def ini(self, request):
+        return HttpResponse("Hello World")
+
+'''
---
+++ SeusPedidos/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+from SeusPedidos.App.views.index import *
+
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'SeusPedidos.views.home', name='home'),
+    # url(r'^SeusPedidos/', include('SeusPedidos.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'$', ini),
+)
