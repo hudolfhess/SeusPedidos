24b893d9eb790b62d76cbfd234fa9cb5e8ade349
hcassus <henriquecassus@gmail.com>
2015-09-17 18:04:37 -0300

    finalizacao da primeira fase das views

---
+++ SeusPedidos/template/App/pedido/pedidoApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by hcassus on 9/17/15.
+ */
+var app = angular.module("pedidoApp", []);
---
+++ SeusPedidos/template/App/pedido/pedidoCtrl.js
@@ -0,0 +1,40 @@
+/**
+ * Created by hcassus on 9/17/15.
+ */
+app.controller("pedidoCtrl", function($scope) {
+
+    $scope.clientes = [
+        {id:1, nome:'Henrique Cassus',email:'henrique.cassus@meuspedidos.com.br'},
+        {id:2, nome:'Juan Ramires',email:'juan@meuspedidos.com.br'},
+        {id:3, nome:'Jonathan Welzel',email:'jonathan.welzel@meuspedidos.com.br'},
+        {id:4, nome:'Hudolf Hess',email:'hudolf@meuspedidos.com.br'}
+    ];
+
+    $scope.pedido = {cliente:undefined, itens:[]};
+
+    $scope.pedidos = [];
+
+    $scope.produtos = [
+        {id:1, nome:"Produto Teste", valor:50.00},
+        {id:2, nome:"Produto Teste 1", valor:60.00},
+        {id:3, nome:"Produto Teste 2", valor:80.00}
+    ];
+
+    $scope.gerarPedido  = function() {
+        pedido = $scope.pedido;
+        $scope.pedidos.push(pedido);
+        $scope.pedido = {};
+    };
+
+    $scope.removerPedido = function(index) {
+        $scope.pedidos.splice(index,1);
+    };
+
+    $scope.adicionarItem = function(){
+        $scope.item.produto = $scope.produtos[$scope.index_produto]
+        item = $scope.item;
+        $scope.pedido.itens.push(item);
+        $scope.item = {};
+    };
+
+});
