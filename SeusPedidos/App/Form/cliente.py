49c76890b7ac6b2b5bfce062a1c21dab398c35e9
Hudolf Hess <hudolf@meuspedidos.com.br>
2015-09-21 10:33:16 -0300

    deleted:    .idea/vcs.xml 	deleted:    .idea/workspace.xml 	deleted:    SeusPedidos/.idea/.name 	deleted:    SeusPedidos/.idea/SeusPedidos.iml 	deleted:    SeusPedidos/.idea/dataSources.ids 	deleted:    SeusPedidos/.idea/dataSources.local.xml 	deleted:    SeusPedidos/.idea/dataSources.xml 	deleted:    SeusPedidos/.idea/dictionaries/hudolfhess.xml 	deleted:    SeusPedidos/.idea/inspectionProfiles/Project_Default.xml 	deleted:    SeusPedidos/.idea/inspectionProfiles/profiles_settings.xml 	deleted:    SeusPedidos/.idea/misc.xml 	deleted:    SeusPedidos/.idea/modules.xml 	deleted:    SeusPedidos/.idea/vagrant.xml 	deleted:    SeusPedidos/.idea/vcs.xml 	deleted:    SeusPedidos/.idea/workspace.xml 	deleted:    SeusPedidos/Api/__init__.pyc 	deleted:    SeusPedidos/Api/views/__init__.pyc 	deleted:    SeusPedidos/Api/views/cliente.pyc 	deleted:    SeusPedidos/Api/views/login.pyc 	deleted:    SeusPedidos/Api/views/produto.pyc 	deleted:    SeusPedidos/App/.DS_Store 	deleted:    SeusPedidos/App/Form/.DS_Store 	deleted:    SeusPedidos/App/Form/__init__.py 	deleted:    SeusPedidos/App/Form/__init__.pyc 	deleted:    SeusPedidos/App/Form/cliente.py 	deleted:    SeusPedidos/App/Form/cliente.pyc 	deleted:    SeusPedidos/App/Form/produto.py 	deleted:    SeusPedidos/App/Form/produto.pyc 	deleted:    SeusPedidos/App/__init__.pyc 	deleted:    SeusPedidos/App/core/__init__.pyc 	deleted:    SeusPedidos/App/core/apiview.pyc 	deleted:    SeusPedidos/App/core/apiviewresult.pyc 	deleted:    SeusPedidos/App/models/__init__.pyc 	deleted:    SeusPedidos/App/models/cliente.pyc 	deleted:    SeusPedidos/App/models/models.pyc 	deleted:    SeusPedidos/App/models/pedido.pyc 	deleted:    SeusPedidos/App/models/produto.pyc 	deleted:    SeusPedidos/App/models/produto_pedido.pyc 	deleted:    SeusPedidos/App/models/user.pyc 	deleted:    SeusPedidos/App/views/__init__.pyc 	deleted:    SeusPedidos/App/views/cliente.pyc 	deleted:    SeusPedidos/App/views/index.pyc 	deleted:    SeusPedidos/App/views/produto.pyc 	deleted:    SeusPedidos/__init__.pyc 	deleted:    SeusPedidos/settings.pyc 	deleted:    SeusPedidos/urls.pyc

--- SeusPedidos/App/Form/cliente.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ClienteForm(forms.Form):
-    nome = forms.CharField(min_length=1,max_length=100)
-    email = forms.EmailField()
--- SeusPedidos/App/Form/produto.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ProdutoForm(forms.Form):
-    nome = forms.CharField(min_length=1,max_length=100)
-    valor = forms.DecimalField(min_value=0)
